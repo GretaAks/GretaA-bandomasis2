7d5fd405b6935a3ff8001a715a39b07e50eee2ca
Greta Aksamitauskaite <atataite@gmail.com>
2021-11-15 00:05:57 +0200

    7: įgalinta animacija ir kortelės

--- js/components/ToyGridComponent.js
+++ js/components/ToyGridComponent.js
@@ -31,11 +31,17 @@ class ToyGridComponent {
     render = () => {
         const {loading,toys} = this.state;
         if (loading) {
-            this.htmlElement.innerHTML = 'siunčiama...';
-        }else {
-            this.htmlElement.innerHTML = 'parsiųsta!';
+            this.htmlElement.innerHTML = `<div class="text-center"><img src="assets/loading.gif"/</div>`;
+        }else if (toys.length > 0) {
+            this.htmlElement.innerHTML = '';
+            const toyElement = toys
+            .map (x => new ToyCardComponent(x))
+            .map (x => x.htmlElement);
+            this.htmlElement.append(...toyElement)
+        }else
+        this.htmlElement = `<h2>Šiuo metu žaislų neturime</h2>`;
         }
     }
-  }
+  
 
 
