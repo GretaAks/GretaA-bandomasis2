2203ced68f27975de1de769ccbed2f53b2b4e986
Greta Aksamitauskaite <atataite@gmail.com>
2021-11-14 22:35:03 +0200

    5: papildytas API

---
+++ js/API.js
@@ -0,0 +1,26 @@
+const baseURL= 'http://localhost:3000';
+
+class API {
+    static getToys = (success,failure) => {
+        fetch(`${baseURL}/toys`)
+        .then (res=> res.json())
+        .then (success)
+        .catch(failure)
+    }
+    static deleteToys= (id,success, failure) => {
+        fetch (`${baseURL}/toys/${id}`, {method:'DELETE'})
+        .then (res => res.ok? success() : failure(res.statusText))
+        .catch(failure)
+    }
+}
+
+//API.getToys(
+//    console.log,
+//    console.error
+//)
+
+//API.deleteToys(
+//    "2",
+//    () => console.log('Ištrinta'),
+//    console.error
+//)
